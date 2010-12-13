a1624efeb967c9c4e70b2cefc85122bdc92413f7
Madiha Mubin <maddymubin@dn0a20fb08.sunet>
2010-12-13 12:20:55 -0800

    Commented file - followed Jon's Twitter example.

--- app/models/facebook_account.rb
+++ app/models/facebook_account.rb
@@ -13,11 +13,20 @@
 #  login          :string(255)
 #  oauth_token    :string(255)
 #
+#
+
+# File:: facebook_account.rb
+#
+# Author::  Jon Boekenoogen (mailto: jboekeno@gmail.com)
+# Commented by::  Madiha Mubin 
+# Copyright::  Copyright (c) 2010 <INSERT_COMPANY_NAME>
 
 require 'oauth'
 require 'model_mixins/account_properties'
 require 'connection'
 
+# This class represents a Facebook Account in the database.
+# For reference, go to https://github.com/nsanta/fbgraph
 class FacebookAccount < ActiveRecord::Base
   include AccountProperties
 
@@ -28,25 +37,54 @@ class FacebookAccount < ActiveRecord::Base
   validates :unique_id, :presence => true, 
     :uniqueness => true
 
+  # Performs a check to see if this account could be valid. 
+  # === Return
+  # true if could be valid, false otherwise
   def authorized?
     !oauth_token.blank?
   end
 
+  # Sets the oauth_token for an account.
+  # Accomplished by asking Facebook for access token given the following
+  # parameters.
+  # ==== Parameters
+  # * +code+ - Facebook App credentials
+  # * +options+ - Html options that are requird by FB api. None for now
+  # === Return
+  # access token from Facebook
   def authorize(code, options = {})
     access_token = client.authorization.process_callback(code, options)
     self.oauth_token = access_token
     access_token
   end
 
-  # Returns an existing account if found
+  # Finds an existing FacebookAccount using unique_id of self
+  # === Return 
+  # Existing account if found, nil otherwise
   def existing
     return self.class.find_by_unique_id(unique_id)
   end
 
+  # Check whether this account and another can be merged
+  # === Parameters
+  # * +other+ - other object to be merged with
+  # === Return 
+  # true if mergeable, false otherwise
   def mergeable(other)
     return unique_id == other.unique_id
   end
 
+  # Accessing Facebook Api to populate contact information into the database.
+  # TODO: Removing connections if no longer friends under ANY accounts.
+  #
+  # === Steps:
+  # * Check if account is valid or can be valid
+  # * Download account's information and their friends' information
+  # * Add friend as Person/FacebookAccount if not in DB
+  # * Add friend as a connection if not in DB
+  # * Special treatment to Significant others, family relationships
+  # === Return
+  # None
   def sync_contacts
     # Check against ActiveRecord validators
     return if !(valid? || authorized?)
@@ -107,6 +145,12 @@ class FacebookAccount < ActiveRecord::Base
     logger.debug "Facebook Sync: Complete #{connection_delta} new connections, before #{old_connection_count} after #{new_connection_count}"
   end
 
+  # Main access point for the FB api. Use this object for all api 
+  # interactions. Client is cached until forced to reload
+  # === Parameters
+  # * +force+ - Force a reload on the client.
+  # === Return 
+  # Facebook Client to use for api access, else nil
   def client(force = false)
     if force || !@client
       @client = FBGraph::Client.new(:client_id => @@config["app_id"],
@@ -116,10 +160,14 @@ class FacebookAccount < ActiveRecord::Base
     @client
   end
 
+  # Convenience metho for accessing configuration parameters.
+  # === Return
+  # Facebook configuration as a YAML hash
   def self.config
     @@config
   end
 
+  # Configuration parameters for Facebook API
   @@config = YAML::load(File.open("#{::Rails.root.to_s}/config/facebook.yml"))
 
 end
